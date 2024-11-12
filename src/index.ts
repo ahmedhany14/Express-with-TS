13f5be96c33c34e06f86728036e0275de494fa75
Ahmed Hany <ah7608867@gmail.com>
2024-11-12 07:29:15 +0200

    sessions|logot|protected routes

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieSession from 'cookie-session';
+
 import { LogInRouter } from './routes/LoginRoutes';
 import bodyParser = require('body-parser');
 dotenv.config({ path: ".config.env" });
@@ -8,9 +10,30 @@ const app = express();
 
 // middlewares
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({
+    keys: ['mysecretkey'],
+    maxAge: 60000,
+}));
+
 app.use(express.json());
 
-
+app.get('/',
+    (request: express.Request, response: express.Response) => {
+        if (request.session?.loggedIn)
+            response.send(`
+                <div>
+                    <div>You are logged in</div>
+                    <a href="/logout">Logout</a>
+                </div>
+            `);
+        else
+            response.send(`
+                <div>
+                    <div>You are not logged in</div>
+                    <a href="/login">Login</a>
+                </div>
+            `);
+    });
 app.use("/", LogInRouter);
 
 const PORT = process.env.PORT || 3000;
--- src/routes/LoginRoutes.ts
+++ src/routes/LoginRoutes.ts
@@ -6,6 +6,13 @@ interface reqBody extends Request {
         password: string;
     }
 }
+
+function requireAuth(request: Request, response: Response, next: Function) {
+    if (request.session?.loggedIn)
+        return next();
+
+    response.status(403).send("Not permitted");
+}
 const router = express.Router();
 router.route("/login")
     .get((request, response) => {
@@ -27,10 +34,22 @@ router.route("/login")
     })
     .post((request: reqBody, response): void => {
         const { email, password } = request.body;
-        if (!email || !password)
-            response.send("Email and Password are required");
-        response.send(`Email: ${email}, Password: ${password}`);
+        if (!email || !password || !(email === "ahmed@hotmail.com" && password === "1234"))
+            response.send("Email or password is incorrect");
+
+        request.session = { loggedIn: true };
+        response.redirect("/");
     })
 
 
+router.route("/logout")
+    .get((request: Request, response: Response) => {
+        request.session = undefined;
+        response.redirect("/");
+    })
+
+router.route("/protected")
+    .get(requireAuth, (request: Request, response: Response) => {
+        response.send("Welcome to protected route, logged in user");
+    });
 export { router as LogInRouter };
